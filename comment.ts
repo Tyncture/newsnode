d34e2afa49201b47ea3f2b39fae173ba6c640d86
John Su <git@tyncture.dev>
2019-08-05 08:04:45 +0100

    Refactor comment coersion logic into separate function

--- comment.ts
+++ comment.ts
@@ -1,37 +1,29 @@
 import { getItem } from "./item";
-import { HNComment } from "./types";
+import { HNComment, HNItem } from "./types";
 
 export async function getComment(id: number): Promise<HNComment> {
-  const item = (await getItem(id)) as HNComment;
-  return {
-    ...item,
-    // Coerce types
-    descendants: Number(item.descendants),
-    parent: item.parent,
-    kids: item.kids ? item.kids.map(cid => Number(cid)) : [],
-    text: item.text,
-    getDescendants: (): Promise<HNComment[]> =>
-      item.kids ? getDescendants(item.kids) : Promise.resolve([]),
-  };
+  const item = await getItem(id);
+  return coerceComment(item);
 }
 
-
-export async function getDescendants(
-  childrenIds: number[],
-): Promise<HNComment[]> {
+export async function getDescendants(childrenIds: number[]): Promise<HNComment[]> {
   const requests = childrenIds.map(id => getItem(id));
-  const descendants = (await Promise.all(requests)).map(item => {
-    const comment = item as HNComment;
-    return {
-      ...comment,
-      /// Coerce types
-      descendants: Number(comment.descendants),
-      parent: comment.parent ? Number(comment.parent) : [],
-      kids: comment.kids ? comment.kids.map(cid => Number(cid)) : [],
-      text: comment.text,
-      getDescendants: () =>
-        comment.kids ? getDescendants(comment.kids) : Promise.resolve([]),
-    };
-  }) as HNComment[];
+  const descendants = (await Promise.all(requests)).map(item =>
+    coerceComment(item),
+  );
   return descendants;
+}
+
+function coerceComment(item: HNItem): HNComment {
+  const comment = item as HNComment;
+  return {
+    ...comment,
+    /// Coerce types
+    descendants: Number(comment.descendants),
+    parent: comment.parent ? Number(comment.parent) : undefined,
+    kids: comment.kids ? comment.kids.map(cid => Number(cid)) : [],
+    text: comment.text,
+    getDescendants: () =>
+      comment.kids ? getDescendants(comment.kids) : Promise.resolve([]),
+  };
 }
