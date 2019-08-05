b39cd3f8b978022d9a46c644803aec3364aa137b
John Su <git@tyncture.dev>
2019-08-06 00:17:16 +0100

    Use default parameter for afterId

--- feeds/new.ts
+++ feeds/new.ts
@@ -4,16 +4,15 @@ import { HNStory } from "../types";
 
 export async function getNewStories(
   limit: number = 100,
-  afterId?: number,
+  afterId: number = 0,
 ): Promise<HNStory[]> {
   const data = await HNApi.getPath("/newstories.json");
   if (Array.isArray(data)) {
     const requests = [];
     // Requests can be slow, so allow for a limit to be set
     for (let i = 0; i < Math.min(data.length, limit); i++) {
-      // Explicitly check against null to account for when ID passed
-      // in is zero. == operator should also match against undefined.
-      if (afterId == null || data[i] > afterId) {
+      // And an ID offset to pickup from last query
+      if (data[i] > afterId) {
         requests.push(getStory(data[i]));
       }
     }
