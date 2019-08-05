49978d78cb7ff21a2c1c6392a363d53d979b96d2
John Su <git@tyncture.dev>
2019-08-05 07:54:49 +0100

    Implement getComment for HNComment

--- comment.ts
+++ comment.ts
@@ -1,6 +1,21 @@
 import { getItem } from "./item";
 import { HNComment } from "./types";
 
+export async function getComment(id: number): Promise<HNComment> {
+  const item = (await getItem(id)) as HNComment;
+  return {
+    ...item,
+    // Coerce types
+    descendants: Number(item.descendants),
+    parent: item.parent,
+    kids: item.kids ? item.kids.map(cid => Number(cid)) : [],
+    text: item.text,
+    getDescendants: (): Promise<HNComment[]> =>
+      item.kids ? getDescendants(item.kids) : Promise.resolve([]),
+  };
+}
+
+
 export async function getDescendants(
   childrenIds: number[],
 ): Promise<HNComment[]> {
