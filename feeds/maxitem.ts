62c034d3829c1bfc73b497398579674ca7a76f34
John Su <git@tyncture.dev>
2019-08-05 23:19:09 +0100

    Export max item feed

--- feeds/maxitem.ts
+++ feeds/maxitem.ts
@@ -2,7 +2,7 @@ import HNApi from "../api";
 import { getItem } from "../models/item";
 import { HNItem } from "../types";
 
-async function getMaxItem(): Promise<HNItem> {
+export async function getMaxItem(): Promise<HNItem> {
   const id = Number(await HNApi.getPath("/maxitem.json"));
   return await getItem(id);
 }
