966acd7cd581a32b117c37f9640ca0eb7345825d
John Su <git@tyncture.dev>
2019-08-05 06:50:34 +0100

    Remove trailing space

---
+++ item.ts
@@ -0,0 +1,15 @@
+import HNApi from "./api";
+import { HNItem } from "./types";
+
+export async function getItem(id: number): Promise<HNItem> {
+  const data = await HNApi.getPath(`/item/${id}`);
+  return {
+    id: Number(data.id),
+    by: data.by,
+    score: Number(data.score),
+    time: new Date(data.time),
+    type: data.type,
+    dead: Boolean(data.dead),
+    deleted: Boolean(data.deleted),
+  }
+}
