1c10878404e65ecdf51b79bee38b6159f3813612
John Su <git@tyncture.dev>
2019-08-05 06:56:26 +0100

    Allow callers of getItem to have access to all specialised properties

--- item.ts
+++ item.ts
@@ -4,6 +4,9 @@ import { HNItem } from "./types";
 export async function getItem(id: number): Promise<HNItem> {
   const data = await HNApi.getPath(`/item/${id}`);
   return {
+    // Let callers have the other properties too
+    ...data,
+    // Coerce types
     id: Number(data.id),
     by: data.by,
     score: Number(data.score),
