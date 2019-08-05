049169e9ce868c3f2addc1ac98f70765a951d37f
John Su <git@tyncture.dev>
2019-08-05 04:52:59 +0100

    Add generic POST function

--- api.ts
+++ api.ts
@@ -4,13 +4,13 @@ const API_URL = "https://hacker-news.firebaseio.com/v0";
 
 async function requestPath(path: string, method: "GET" | "POST", body?: {}) {
   try {
-    const response= await Axios.request({
+    const response = await Axios.request({
       method,
       headers: {
         "Content-Type": "application/json",
       },
       data: JSON.stringify(body),
-      url: `${API_URL}${path}`
+      url: `${API_URL}${path}`,
     });
     return response.data;
   } catch (e) {
@@ -18,7 +18,10 @@ async function requestPath(path: string, method: "GET" | "POST", body?: {}) {
   }
 }
 
-
 export async function getPath(path: string) {
   return await requestPath(path, "GET");
+}
+
+export async function postPath(path: string, body?: {}) {
+  return await requestPath(path, "POST", body);
 }
