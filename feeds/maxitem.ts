fc6624097a191f265c2b5c528abc86d593190eef
John Su <git@tyncture.dev>
2019-08-05 22:47:43 +0100

    Implement /maxitem binding

---
+++ feeds/maxitem.ts
@@ -0,0 +1,8 @@
+import HNApi from "../api";
+import { getItem } from "../models/item";
+import { HNItem } from "../types";
+
+async function getMaxItem(): Promise<HNItem> {
+  const id = Number(await HNApi.getPath("/maxitem.json"));
+  return await getItem(id);
+}
