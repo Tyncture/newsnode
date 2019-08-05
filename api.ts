4e74a93f10150f6f356443730f3d2c2a36561c7c
John Su <git@tyncture.dev>
2019-08-05 06:37:47 +0100

    Remove redundant error handling block

--- api.ts
+++ api.ts
@@ -3,19 +3,17 @@ import Axios from "axios";
 const API_URL = "https://hacker-news.firebaseio.com/v0";
 
 async function requestPath(path: string, method: "GET" | "POST", body?: {}): Promise<{}> {
-  try {
-    const response = await Axios.request({
-      method,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: JSON.stringify(body),
-      url: `${API_URL}${path}`,
-    });
-    return response.data;
-  } catch (e) {
-    throw e;
-  }
+  const response = await Axios.request({
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify(body),
+    url: `${API_URL}${path}`,
+  });
+  return response.data;
+  // Axios will reject promise if non 200 OK response
+  // Error handling is delegated to the implementor
 }
 
 async function getPath(path: string) {
