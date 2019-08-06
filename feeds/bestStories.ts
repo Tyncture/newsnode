f5f3310fa2b3e2de36d9e320ace39f2f8214b2e4
John Su <git@tyncture.dev>
2019-08-06 03:19:32 +0100

    Implement getBestStories

---
+++ feeds/bestStories.ts
@@ -0,0 +1,9 @@
+import { HNStory } from "../types";
+import { getFeed } from "./feed";
+
+export async function getBestStories(
+  limit: number = 100,
+  afterId: number = 0,
+): Promise<HNStory[]> {
+  return (await getFeed("beststories", limit, afterId)) as HNStory[];
+}
