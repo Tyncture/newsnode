c9f7fd71a83aa1dff6ccdc42dc448bcd14a9f73d
John Su <git@tyncture.dev>
2019-08-06 03:37:29 +0100

    Return proper types for HNAsk

--- feeds/askStories.ts
+++ feeds/askStories.ts
@@ -1,9 +1,9 @@
-import { HNStory } from "../types";
+import { HNAsk } from "../types";
 import { getFeed } from "./feed";
 
 export async function getAskStories(
   limit: number = 100,
   afterId: number = 0,
-): Promise<HNStory[]> {
-  return (await getFeed("askstories", limit, afterId)) as HNStory[];
+): Promise<HNAsk[]> {
+  return (await getFeed("askstories", limit, afterId)) as HNAsk[];
 }
