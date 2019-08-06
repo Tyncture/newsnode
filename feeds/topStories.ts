942bbd9de6719d89393dbbefab88d1e5ac01111f
John Su <git@tyncture.dev>
2019-08-06 03:17:54 +0100

    Implement getTopStories

--- feeds/topStories.ts
+++ feeds/topStories.ts
@@ -1,9 +1,9 @@
 import { HNStory } from "../types";
 import { getFeed } from "./feed";
 
-export async function getNewStories(
+export async function getTopStories(
   limit: number = 100,
   afterId: number = 0,
 ): Promise<HNStory[]> {
-  return await getFeed("newstories", limit, afterId) as HNStory[];
+  return (await getFeed("topstories", limit, afterId)) as HNStory[];
 }
