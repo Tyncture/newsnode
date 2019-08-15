fe0e5340365ed26f54b090fed4026a5032c58505
John Su <git@tyncture.dev>
2019-08-15 23:10:24 +0100

    Add resilience against new post IDs not yet made available by the API due to latency

--- src/feeds/feed.ts
+++ src/feeds/feed.ts
@@ -7,17 +7,29 @@ export async function getFeed(
   limit: number = 100,
   afterId: number = 0,
 ): Promise<HNPost[]> {
-  const data = await HNApi.getPath(`/${feed}.json`);
-  if (Array.isArray(data)) {
+  const posts = [] as HNPost[];
+  const feedData = await HNApi.getPath(`/${feed}.json`);
+  if (Array.isArray(feedData)) {
     const requests = [];
     // Requests can be slow, so allow for a limit to be set
-    for (let i = 0; i < Math.min(data.length, limit); i++) {
+    for (let i = 0; i < Math.min(feedData.length, limit); i++) {
       // And an ID offset to pickup from last query
-      if (data[i] > afterId) {
-        requests.push(getPost(data[i]));
+      if (feedData[i] > afterId) {
+        requests.push((async () => {
+          // If the post is too recent, the API may return a 404,
+          // so only push to array if not null/undefined
+          const postData = await getPost(feedData[i]);
+          if (postData) {
+            posts.push(postData);
+          }
+        })());
+        // Anonymous async functions do not immediately return
+        // promises without giving them a call first
       }
     }
-    return Promise.all(requests);
+    // Wait for all requests to complete
+    await Promise.all(requests);
+    return posts;
   }
   return [];
 }
